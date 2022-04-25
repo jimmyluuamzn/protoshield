4fce2adecae515ad75ca70490d56f81d8c71cfc9
Jimmy Lu <jimmyluu@amazon.com>
2022-04-25 16:06:39 -0400

    minor changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+        <h1>SHIELD IS THE MASTER</h1>
       </header>
     </div>
   );
